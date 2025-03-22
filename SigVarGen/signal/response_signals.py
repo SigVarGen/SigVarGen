2748b7807dc1d850fc20caaf5ed9ba33357e71ac
ovakulen <ovakulen@uwaterloo.ca>
2025-03-21 22:47:36 -0400

    BUG: fixing disperse offset

--- SigVarGen/signal/response_signals.py
+++ SigVarGen/signal/response_signals.py
@@ -154,14 +154,21 @@ def apply_interrupt_modifications(
     """
     if disperse:
         if not drop:
+            # Center interrupt around the base part mean
+            inter_mean = np.mean(inter_part)
+            base_mean = 0.8*np.mean(base_part)
+            inter_part += (base_mean - inter_mean)
             allowed_drift = device_max - np.max(inter_part)
             allowed_drift = max(allowed_drift, 0)
-            min_drift = np.max(inter_part)
+            min_drift = 0 #np.max(inter_part)
             inter_part = apply_baseline_drift_middle_peak(inter_part, allowed_drift, direction='up', min_drift=min_drift)
         else:
+            inter_mean = 0.8*np.mean(inter_part)
+            base_mean = np.mean(base_part)
+            inter_part += (base_mean - inter_mean)
             allowed_drift = np.min(inter_part) - device_min
             allowed_drift = max(allowed_drift, 0)
-            min_drift = device_min
+            min_drift = 0 #device_min
             inter_part = apply_baseline_drift_middle_peak(inter_part, allowed_drift, direction='down', min_drift=min_drift)
 
     # Compute the current interrupt range
@@ -499,7 +506,7 @@ def add_complexity_to_inter(
         device_min=min(INTERRUPT_RANGES[domain]['amplitude'][0], DEVICE_RANGES[domain]['amplitude'][0]),
         device_max=max(INTERRUPT_RANGES[domain]['amplitude'][1], DEVICE_RANGES[domain]['amplitude'][1]),
         drop=drop,
-        disperse=False, 
+        disperse=True, 
         blend_factor=blend_factor
     )
 
